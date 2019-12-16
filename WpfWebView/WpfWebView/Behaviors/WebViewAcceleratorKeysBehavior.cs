ad264a674fdf0bbe16c45b0226c571c39b80a414
Martin Vega <mvegaca@outlook.com>
2019-12-16 16:58:17 +0100

    Add AcceleratorKeys to WebView

---
+++ WpfWebView/WpfWebView/Behaviors/WebViewAcceleratorKeysBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using Microsoft.Toolkit.Wpf.UI.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace WpfWebView.Behaviors
+{
+    public class WebViewAcceleratorKeysBehavior : Behavior<WebView>
+    {
+        private ModifierKeys _modifierKey = ModifierKeys.None;
+
+        public InputBindingCollection WebViewInputBindings
+        {
+            get { return (InputBindingCollection)GetValue(WebViewInputBindingsProperty); }
+            set { SetValue(WebViewInputBindingsProperty, value); }
+        }
+
+        public static readonly DependencyProperty WebViewInputBindingsProperty = DependencyProperty.Register("WebViewInputBindings", typeof(InputBindingCollection), typeof(WebViewAcceleratorKeysBehavior), new PropertyMetadata(new InputBindingCollection()));
+
+        public WebViewAcceleratorKeysBehavior()
+        {
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.AcceleratorKeyPressed += OnAcceleratorKeyPressed;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.AcceleratorKeyPressed -= OnAcceleratorKeyPressed;
+        }
+
+        private void OnAcceleratorKeyPressed(object sender, WebViewControlAcceleratorKeyPressedEventArgs e)
+        {
+            if (e.EventType != CoreAcceleratorKeyEventType.KeyDown
+                && e.EventType != CoreAcceleratorKeyEventType.SystemKeyDown
+                && WebViewInputBindings.Count == 0)
+            {
+                return;
+            }
+
+            e.Handled = true;
+            if (e.VirtualKey.IsModifierKey())
+            {
+                _modifierKey = e.VirtualKey.GetModifierKey();
+                return;
+            }
+
+            if (Enum.TryParse(e.VirtualKey.ToString(), out Key key))
+            {
+                foreach (KeyBinding keyBinding in WebViewInputBindings)
+                {
+                    if (_modifierKey == keyBinding.Modifiers && key == keyBinding.Key)
+                    {
+                        keyBinding.Command.Execute(keyBinding.CommandParameter);
+                    }
+                }
+            }
+
+            _modifierKey = ModifierKeys.None;
+        }
+    }
+}
---
+++ WpfWebView/WpfWebView/Helpers/VirtualKeyExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+
+namespace Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT
+{
+    public static class VirtualKeyExtensions
+    {
+        public static bool IsModifierKey(this VirtualKey virtualKey)
+            => virtualKey.GetModifierKey() != ModifierKeys.None;
+        public static ModifierKeys GetModifierKey(this VirtualKey virtualKey)
+        {
+            switch (virtualKey)
+            {
+                case VirtualKey.Menu:
+                case VirtualKey.LeftMenu:
+                case VirtualKey.RightMenu:
+                    return ModifierKeys.Alt;
+                case VirtualKey.Control:
+                case VirtualKey.LeftControl:
+                case VirtualKey.RightControl:
+                    return ModifierKeys.Control;
+                case VirtualKey.Shift:
+                case VirtualKey.LeftShift:
+                case VirtualKey.RightShift:
+                    return ModifierKeys.Shift;
+                case VirtualKey.LeftWindows:
+                case VirtualKey.RightWindows:
+                    return ModifierKeys.Windows;
+                default:
+                    return ModifierKeys.None;
+            }
+        }
+    }
+}
